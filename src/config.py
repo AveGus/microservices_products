2a30d20ae50c3fd849527a7933d0b3ea74314c80
AveGus(Eldar) <garipov.eldar200@gmail.com>
2024-09-23 01:18:37 +0500

    add defaults db value

--- src/config.py
+++ src/config.py
@@ -3,11 +3,11 @@ import os
 
 
 load_dotenv()
-DB_HOST = os.getenv("DB_HOST")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_NAME = os.getenv("DB_NAME")
-DB_PORT = os.getenv("DB_PORT")
+DB_HOST = os.getenv("DB_HOST", default="0.0.0.0")
+DB_USER = os.getenv("DB_USER", default="test_db")
+DB_PASS = os.getenv("DB_PASS", default="test_db")
+DB_NAME = os.getenv("DB_NAME", default="test_db")
+DB_PORT = os.getenv("DB_PORT", default="5432")
 
 DB_HOST_TEST = os.getenv("DB_HOST_TEST", default="0.0.0.0")
 DB_USER_TEST = os.getenv("DB_USER_TEST", default="test_db")
